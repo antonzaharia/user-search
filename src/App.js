d47f345305ece0078922f6dd947a617321ed74ef
Anton Zaharia <zaharia_anton@yahoo.com>
2022-11-15 18:58:07 +0000

    Implemented App base layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-// JSON file
-// https://raw.githubusercontent.com/klausapp/frontend-engineer-test-task/master/users.json
+import Header from './components/Header'
+import { UsersProvider } from './contexts'
+import UsersPanel from './components/UsersPanel'
 
 function App() {
   return (
-    <div className="App container mx-auto max-w-[1200px]">
-      <header className="">Account users</header>
-    </div>
+    <UsersProvider>
+      <div className="App container mx-auto max-w-[1200px]">
+        <Header />
+        <UsersPanel />
+      </div>
+    </UsersProvider>
   )
 }
 
