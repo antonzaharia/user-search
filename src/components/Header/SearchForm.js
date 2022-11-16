563a38c98a4c9f39658f243ddfddb45e051f5d6d
Anton Zaharia <zaharia_anton@yahoo.com>
2022-11-16 08:57:44 +0000

    debouncer for searching

--- src/components/Header/SearchForm.js
+++ src/components/Header/SearchForm.js
@@ -1,31 +1,38 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useCallback } from 'react'
 import { Users } from '../../contexts'
+import { debounce } from '../../utils'
 
 function SearchForm() {
   const { fetchUsers, setAllUsers } = useContext(Users)
   const [searchTerm, setSearchTerm] = useState('')
 
-  const regex = new RegExp(searchTerm, 'gi')
-
-  var filterBySearchTerm = (usersBulk) =>
-    usersBulk.filter((user) => {
+  const filterBySearchTerm = (usersBulk, term) => {
+    let regex = new RegExp(term, 'gi')
+    return usersBulk.filter((user) => {
       return regex.test(user.name) || regex.test(user.email) || regex.test(user.role)
     })
+  }
 
   const handleSearchTermChange = (event) => {
     setSearchTerm(event.target.value)
   }
 
-  const handleSearch = async (event) => {
+  const triggerSearch = async (term) => {
+    console.log(term)
     const result = await fetchUsers()
-    let newUsers = filterBySearchTerm(result)
+    let newUsers = filterBySearchTerm(result, term)
 
     setAllUsers(newUsers)
   }
 
+  const debounceSearch = useCallback(
+    debounce((term) => triggerSearch(term), 500),
+    []
+  )
+
   return (
     <form>
-      <input type="text" value={searchTerm} onChange={handleSearchTermChange} onKeyUp={handleSearch} />
+      <input type="text" value={searchTerm} onChange={handleSearchTermChange} onKeyUp={() => debounceSearch(searchTerm)} />
     </form>
   )
 }
---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+const debounce = (func, delay) => {
+  let timer
+  return function () {
+    let self = this
+    let args = arguments
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      func.apply(self, args)
+    }, delay)
+  }
+}
+
+export { debounce }
