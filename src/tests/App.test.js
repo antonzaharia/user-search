5b702503061e965fcdbdf3fcbfd110687b100145
Anton Zaharia <zaharia_anton@yahoo.com>
2022-11-17 08:29:27 +0000

    fixed tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from '../App'
 
 test('renders account users page title', () => {
   render(<App />)
--- src/tests/UsersTable.test.js
+++ src/tests/UsersTable.test.js
@@ -1,13 +1,17 @@
 import { render, screen } from '@testing-library/react'
-import UsersTable from './components/UsersPanel/UsersTable'
+import UsersTable from '../components/UsersPanel/UsersTable'
+import { CheckedProvider } from '../contexts/checkedUsers'
 
 test('renders the users table', () => {
   render(
-    <UsersTable users={[{ id: 1, name: 'Anton', email: 'test@test.com', avatar: 'https://noimage.com', role: 'ADMIN' }]} />
+    <CheckedProvider>
+      <UsersTable users={[{ id: 1, name: 'Anton', email: 'test@test.com', avatar: 'https://noimage.com', role: 'ADMIN' }]} />
+    </CheckedProvider>
   )
   const name = screen.getByText(/Anton/i)
   const email = screen.getByText(/test@test.com/i)
   const role = screen.getByText(/Admin/i)
+
   expect(name).toBeInTheDocument()
   expect(email).toBeInTheDocument()
   expect(role).toBeInTheDocument()
